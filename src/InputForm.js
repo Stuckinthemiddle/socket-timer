08356a24e04553486c6f8f72ee1d1a0b0d7f21bb
Todd Kegley <todd.kegley@gmail.com>
2017-10-28 12:14:06 -0500

    input is now controlled component

--- src/InputForm.js
+++ src/InputForm.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
-class InputForm extends React.Component {
+class InputForm extends Component {
+    state ={interval: ''};
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
     render() {
         return (
-            <form>
-                <input type="text" placeholder="interval in milliseconds" />
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" 
+                    placeholder="interval in milliseconds" 
+                    value={this.state.interva}
+                    onChange={(event) => this.setState({interval: event.target.value}) }/>
                 <button type="submit">Subscribe</button>
             </form>
         );
