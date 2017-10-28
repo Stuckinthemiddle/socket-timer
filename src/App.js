b6651f45a9bfda788160048ec75dfa62009d0368
Todd Kegley <todd.kegley@gmail.com>
2017-10-28 11:25:45 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {subscribeToTimer} from './api.js';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        timestamp: 'no timestamp messages yet ...'
+    };
+    console.log('calling subscribeToTimer');
+    const callback = (err, timestamp) => { 
+      this.setState ({timestamp});
+    };
+
+    subscribeToTimer((err, timestamp) => { 
+      this.setState ({timestamp});
+    });
+    console.log('subscribed to timer');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          Timer value from server: {this.state.timestamp}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,15 @@
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:8000');
+
+function subscribeToTimer(cb, interval) {
+    debugger;
+    console.log('entered subscribeToTimer');
+    // register handler for timer event
+    socket.on('timer', timestamp => cb (null, timestamp));
+    // send (emit) event to server - subscribe to receive
+    // timer events every 1000 ms
+
+    socket.emit('subscribeToTimer', 1000);
+}
+
+export { subscribeToTimer }
